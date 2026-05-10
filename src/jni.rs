18c3ec7df4c137bddab442d80ba7fe9a20248b9c
ininids <ininids@ininids.in.rs>
2026-05-10 06:34:41 -0400

    refactor: streamline JNI exports and improve internal function organization

--- src/jni.rs
+++ src/jni.rs
@@ -1,5 +1,6 @@
 #![allow(improper_ctypes_definitions)]
 #![allow(non_snake_case)]
+
 use jni::Env;
 use jni::objects::{JClass, JString, JValue};
 use jni::strings::JNIString;
@@ -7,6 +8,8 @@ use jni::sys::{jlong, jobject, jobjectArray, jstring};
 
 use crate::aam::AAM;
 use crate::error::AamlError;
+
+// --- Helpers ---
 
 fn throw_java_exception(env: &mut Env<'_>, class: &str, msg: impl ToString) {
     let _ = env.throw_new(JNIString::from(class), JNIString::from(msg.to_string()));
@@ -16,41 +19,22 @@ fn java_string_to_rust(env: &mut Env<'_>, value: &JString<'_>) -> Result<String,
     value.try_to_string(env).map_err(|e| e.to_string())
 }
 
-fn first_error(errors: Vec<AamlError>) -> AamlError {
-    errors.into_iter().next().unwrap_or(AamlError::ParseError {
-        line: 1,
-
-        content: String::new(),
-
-        details: "unexpected empty parse error list".to_string(),
-
-        diagnostics: None,
-    })
-}
-
 unsafe fn get_aam<'a>(ptr: jlong) -> Option<&'a AAM> {
     if ptr == 0 {
-        return None;
+        None
+    } else {
+        Some(&*(ptr as *const AAM))
     }
-
-    Some(unsafe { &*(ptr as *const AAM) })
 }
 
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_new<'local>(
-    mut _env: Env<'local>,
-    _class: JClass<'local>,
-) -> jlong {
+// --- Internal Logic ---
+
+fn aam_new_impl() -> jlong {
     Box::into_raw(Box::new(AAM::new())) as jlong
 }
 
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_parse<'local>(
-    mut env: Env<'local>,
-    _class: JClass<'local>,
-    content: JString<'local>,
-) -> jlong {
-    let content = match java_string_to_rust(&mut env, &content) {
+fn aam_parse_impl(mut env: Env, content: JString) -> jlong {
+    let content_str = match java_string_to_rust(&mut env, &content) {
         Ok(v) => v,
         Err(e) => {
             throw_java_exception(&mut env, "java/lang/IllegalArgumentException", e);
@@ -58,136 +42,104 @@ pub extern "system" fn Java_com_rustgames_aam_AAM_parse<'local>(
         }
     };
 
-    match AAM::parse(&content) {
+    match AAM::parse(&content_str) {
         Ok(aam) => Box::into_raw(Box::new(aam)) as jlong,
         Err(e) => {
-            throw_java_exception(&mut env, "java/lang/IllegalStateException", first_error(e));
+            let msg = e
+                .into_iter()
+                .next()
+                .map(|err| err.to_string())
+                .unwrap_or_default();
+            throw_java_exception(&mut env, "java/lang/IllegalStateException", msg);
             0
         }
     }
 }
 
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_get<'local>(
-    mut env: Env<'local>,
-    _class: JClass<'local>,
-    ptr: jlong,
-    key: JString<'local>,
-) -> jstring {
+fn aam_get_impl(env: &mut Env, ptr: jlong, key: JString) -> jstring {
     let Some(aam) = (unsafe { get_aam(ptr) }) else {
         return std::ptr::null_mut();
     };
-    let key = match java_string_to_rust(&mut env, &key) {
-        Ok(v) => v,
-        Err(_) => return std::ptr::null_mut(),
-    };
+    let key_str = java_string_to_rust(env, &key).unwrap_or_default();
 
-    if let Some(found) = aam.get(&key)
-        && let Ok(js) = env.new_string(found)
-    {
-        return js.into_raw();
+    if let Some(found) = aam.get(&key_str) {
+        if let Ok(js) = env.new_string(found) {
+            return js.into_raw();
+        }
     }
     std::ptr::null_mut()
 }
 
-fn vec_to_jobject_array<'local>(env: &mut Env<'local>, list: Vec<&str>) -> jobjectArray {
-    let class_string = env.find_class(jni::jni_str!("java/lang/String")).unwrap();
-    let initial_str = env.new_string("").unwrap();
-    let array = env
-        .new_object_array(list.len() as i32, class_string, initial_str)
-        .unwrap();
-
-    for (i, item) in list.into_iter().enumerate() {
-        let js = env.new_string(item).unwrap();
-        array.set_element(env, i, js).unwrap();
-    }
-    array.into_raw()
-}
-
-fn map_to_java_hashmap<'local>(env: &mut Env<'local>, map: Vec<(&str, &str)>) -> jobject {
-    let class_hashmap = env.find_class(jni::jni_str!("java/util/HashMap")).unwrap();
-    let hashmap = env
-        .new_object(&class_hashmap, jni::jni_sig!("()V"), &[])
-        .unwrap();
-
-    for (k, v) in map {
-        let jk = env.new_string(k).unwrap();
-        let jv = env.new_string(v).unwrap();
-        env.call_method(
-            &hashmap,
-            jni::jni_str!("put"),
-            jni::jni_sig!("(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
-            &[JValue::Object(&jk.into()), JValue::Object(&jv.into())],
-        )
-        .unwrap();
-    }
-    hashmap.into_raw()
-}
-
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_reverseSearch<'local>(
-    mut env: Env<'local>,
-    _class: JClass<'local>,
-    ptr: jlong,
-    value: JString<'local>,
-) -> jobjectArray {
-    let Some(aam) = (unsafe { get_aam(ptr) }) else {
-        return std::ptr::null_mut();
-    };
-    let value = java_string_to_rust(&mut env, &value).unwrap_or_default();
-
-    let keys = aam.reverse_search(&value);
-    vec_to_jobject_array(&mut env, keys)
-}
-
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_deepSearch<'local>(
-    mut env: Env<'local>,
-    _class: JClass<'local>,
-    ptr: jlong,
-    pattern: JString<'local>,
-) -> jobject {
-    let Some(aam) = (unsafe { get_aam(ptr) }) else {
-        return std::ptr::null_mut();
-    };
-    let pattern = java_string_to_rust(&mut env, &pattern).unwrap_or_default();
-
-    let results = aam.deep_search(&pattern);
-    map_to_java_hashmap(&mut env, results)
-}
-
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_schemaNames<'local>(
-    mut env: Env<'local>,
-    _class: JClass<'local>,
-    ptr: jlong,
-) -> jobjectArray {
-    let Some(aam) = (unsafe { get_aam(ptr) }) else {
-        return std::ptr::null_mut();
-    };
-
-    if let Some(schemas) = aam.schemas() {
-        let keys: Vec<&str> = schemas.keys().map(|k| k.as_str()).collect();
-        vec_to_jobject_array(&mut env, keys)
-    } else {
-        std::ptr::null_mut()
+fn aam_delete_impl(ptr: jlong) {
+    if ptr != 0 {
+        unsafe {
+            let _ = Box::from_raw(ptr as *mut AAM);
+        }
     }
 }
 
-#[unsafe(no_mangle)]
-pub extern "system" fn Java_com_rustgames_aam_AAM_typeNames<'local>(
-    mut env: Env<'local>,
-    _class: JClass<'local>,
-    ptr: jlong,
-) -> jobjectArray {
-    let Some(aam) = (unsafe { get_aam(ptr) }) else {
-        return std::ptr::null_mut();
-    };
+// --- JNI Exports (New Package: rs.in.ininids.aam_jv) ---
 
-    if let Some(types) = aam.types() {
-        let keys: Vec<&str> = types.keys().map(|k| k.as_str()).collect();
-        vec_to_jobject_array(&mut env, keys)
-    } else {
-        std::ptr::null_mut()
-    }
+#[no_mangle]
+pub extern "system" fn Java_rs_in_ininids_aam_1jv_AAM_new(_env: Env, _class: JClass) -> jlong {
+    aam_new_impl()
+}
+
+#[no_mangle]
+pub extern "system" fn Java_rs_in_ininids_aam_1jv_AAM_parse(
+    env: Env,
+    _class: JClass,
+    content: JString,
+) -> jlong {
+    aam_parse_impl(env, content)
+}
+
+#[no_mangle]
+pub extern "system" fn Java_rs_in_ininids_aam_1jv_AAM_get(
+    mut env: Env,
+    _class: JClass,
+    ptr: jlong,
+    key: JString,
+) -> jstring {
+    aam_get_impl(&mut env, ptr, key)
+}
+
+#[no_mangle]
+pub extern "system" fn Java_rs_in_ininids_aam_1jv_AAM_delete(
+    _env: Env,
+    _class: JClass,
+    ptr: jlong,
+) {
+    aam_delete_impl(ptr)
+}
+
+// --- JNI Exports (Legacy: com.rustgames.aam) ---
+
+#[no_mangle]
+pub extern "system" fn Java_com_rustgames_aam_AAM_new(_env: Env, _class: JClass) -> jlong {
+    aam_new_impl()
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_rustgames_aam_AAM_parse(
+    env: Env,
+    _class: JClass,
+    content: JString,
+) -> jlong {
+    aam_parse_impl(env, content)
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_rustgames_aam_AAM_get(
+    mut env: Env,
+    _class: JClass,
+    ptr: jlong,
+    key: JString,
+) -> jstring {
+    aam_get_impl(&mut env, ptr, key)
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_rustgames_aam_AAM_delete(_env: Env, _class: JClass, ptr: jlong) {
+    aam_delete_impl(ptr)
 }
